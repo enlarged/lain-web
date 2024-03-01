01307f6c2ce5d5657c1017f1aa2697112447fb89
enlarged <151495048+enlarged@users.noreply.github.com>
2024-03-01 13:21:46 -0600

    Delete commands.js

--- commands.js
+++
@@ -1,9 +0,0 @@
-function navigation(module) {
-    var tabs = document.querySelectorAll('.tab');
-    tabs.forEach(tab => tab.classList.remove('active'));
-    
-    var tab = document.getElementById(module);
-    if (tab) {
-        tab.classList.add('active');
-    }
-}
